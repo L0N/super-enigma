ff7f1323380375807c925a103d18305136cb2d02
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-11 14:36:55 +1000

    basic header footer content

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,14 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <footer>
+                <div>
+                    Please come by again
+                </div>
+                <div>
+                    Copyright 2020
+                </div>
+            </footer>
         )
     }
 }
