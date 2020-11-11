bfc54f345fb9b43b6b98ca608f998f6f1a19aac6
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-11 14:38:43 +1000

    restore webvitals

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Body extends Component {
     render() {
         return (
-            <div>
+            <div className='Body'>
                 
             </div>
         )
