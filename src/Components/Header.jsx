75a6f189c5d9d2d6e3d315b1a7f48f10fea0f875
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-11 14:46:04 +1000

    update header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <div>
-                <h1>Libary</h1>
+                <h1>Libary of books read</h1>
             </div>
         )
     }
