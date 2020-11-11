d2c56d3e3967d04284a86e81fb546250fa4ff257
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-11 14:33:02 +1000

    include component skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Header from './Components/Header';
+import Body from "./Components/Body";
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        < Header />
       </header>
+      < Body />
+      < Footer />
     </div>
   );
 }
